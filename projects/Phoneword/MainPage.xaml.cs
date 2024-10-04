c4d75152d8d4c628ab8b1b64bbc8167a232c992e
harun3543 <harun.3543@gmail.com>
2024-10-04 17:44:42 +0300

    10.04.2024 - XAML markup extension uygulandı

---
+++ projects/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿namespace Phoneword
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        string translatedNumber;
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = "Call" + translatedNumber;
+
+            }
+            else
+            {
+                CallButton.IsEnabled = false;
+            }
+        }
+
+        async void OnCall(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                try
+                {
+                    if (PhoneDialer.Default.IsSupported)
+                    {
+                        PhoneDialer.Default.Open(translatedNumber);
+                    }
+                }
+                catch (ArgumentException)
+                {
+
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (Exception)
+                {
+                    // diğer hata almalarda
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
+
+            }
+        }
+    }
+}
